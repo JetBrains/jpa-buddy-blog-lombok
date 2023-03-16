bba87e27c081ff3a3b00cd62273c8b6eac8950e1
Georgii Vlasov <honest.niceman@gmail.com>
2023-03-16 10:21:51 +0400

    Init commit

---
+++ src/main/java/com/example/lombokpitfalls/LombokPitfallsApplication.java
@@ -0,0 +1,14 @@
+package com.example.lombokpitfalls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokPitfallsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LombokPitfallsApplication.class,
+                              args);
+    }
+
+}
---
+++ src/test/java/com/example/lombokpitfalls/LombokPitfallsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lombokpitfalls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LombokPitfallsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
